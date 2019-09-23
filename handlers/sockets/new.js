5061da9ffc7a24402d10754b611c5ce7c3d56019
yandere chan <51772725+yc8690347@users.noreply.github.com>
2019-09-23 18:11:48 -0400

    Update new.js

--- handlers/sockets/new.js
+++ handlers/sockets/new.js
@@ -4,7 +4,7 @@ function exec(info, io, chalk, fs) {
     console.log(chalk.blue(`Arch: ${info.arch}`));
     console.log(chalk.blue(`IP: ${info.ip}`));
     console.log(chalk.blue(`Hostname: ${info.hostname}`));
-    fs.appendFileSync('../.logs/logs.txt', `New user: ${info.platform}, ${info.arch}, ${info.ip}, ${info.hostname}.`);
+    fs.appendFileSync('../.logs/logs.txt', `\nNew user: ${info.platform}, ${info.arch}, ${info.ip}, ${info.hostname}.`);
     io.emit('new', info);
 }
 
