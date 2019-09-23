7b250eaf2fc363f1258b52e8ae0529b4c1581a1a
yandere chan <51772725+yc8690347@users.noreply.github.com>
2019-09-23 18:12:35 -0400

    Update ping.js

--- handlers/sockets/ping.js
+++ handlers/sockets/ping.js
@@ -1,7 +1,7 @@
 function exec(info, chalk, fs) {
     console.log(chalk.green('Pong! from:'));
     console.log(chalk.blue(info.hostname));
-    fs.appendFileSync('../.logs/logs.txt', `Pinged from ${info.hostname}`);
+    fs.appendFileSync('../.logs/logs.txt', `\nPinged from ${info.hostname}`);
 }
 
 module.exports = exec;
