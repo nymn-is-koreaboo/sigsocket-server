61ca7c31dd5d2fa041bc3c636522ab0e5feef16f
yandere chan <51772725+yc8690347@users.noreply.github.com>
2019-09-23 18:13:00 -0400

    Update scr.js

--- handlers/sockets/scr.js
+++ handlers/sockets/scr.js
@@ -1,7 +1,7 @@
 function exec(info, io, chalk, fs) {
     console.log(chalk.green('New image received with Base64 data:'));
     console.log(chalk.blue(info));
-    fs.appendFileSync('../.logs/logs.txt', 'Image received from client.');
+    fs.appendFileSync('../.logs/logs.txt', '\nImage received from client.');
     io.emit('scr', info);
 }
 
